ec7ee90c09e4ac3bf443383c6f569a98eb3405e2
Kenneth Jung <jungeu1509@gmail.com>
2020-04-21 20:34:33 +0900

    Create 2775(want_to_be_women_president).cpp

---
+++ 2775(want_to_be_women_president).cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+#define MAXVAL 15
+int apt[MAXVAL][MAXVAL] = {0, };
+
+int count_people(int k, int n);
+
+int main()
+{
+	int T, k, n;
+	for(int i = 1; i< MAXVAL; i++)
+		apt[0][i] = i;
+
+	cin >> T;
+	
+	for(int j=0; j<T; j++)
+	{
+		cin >> k >> n;
+		cout << count_people(k, n) << "\n";
+	}
+	return 0;
+}
+
+int count_people(int k, int n)
+{
+	if(apt[k][n]!=0)
+		return apt[k][n];
+		
+	for(int i = 1; i<=n; i++)
+	{
+		apt[k][n] += count_people(k-1, i);
+	}
+	
+	return apt[k][n];
+	
+}
