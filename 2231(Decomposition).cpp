980faa3e61de56515cda2186ec96ae870ef1032c
Kenneth Jung <jungeu1509@gmail.com>
2020-06-16 14:02:26 +0900

    Update 2231(Decomposition).cpp

--- 2231(Decomposition).cpp
+++ 2231(Decomposition).cpp
@@ -3,38 +3,36 @@ using namespace std;
 
 int main()
 {
-  long N, temp, sum = 0, i;
-  int digit = 0;
-  bool find = 0;
-
+  int N, temp, sum = 0, i, digit = 0;
   cin >> N;
-    
   temp = N;
+  
   while(temp)
   {
     temp /= 10;
     digit++;
   }
-
+  
   for(i = (N - digit * 9); i <= N; ++i)
   {
-    sum = i;
-    temp = i;
-
+    if(i < 1)
+      continue;
+    
+    sum = temp = i;
+    
     while(temp)
     {
       sum += (temp % 10);
       temp /= 10;
     }
+    
     if(sum == N)
-    {
-        find = 1;
-        cout << i << '\n';
-        return 0;
-    }
+		{
+      cout << i;
+			return 0;
+		}
   }
-
-  if(find == 0)
-    cout << '0' << '\n';
+  
+  cout << '0';
   return 0;
 }
