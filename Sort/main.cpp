a2a43ec8ad613f8eb20d3c0906ba7f2ebf3e029b
Eunwoo Jung <jungeu1509@gmail.com>
2021-02-02 16:38:20 +0900

    add MergeSort

--- Sort/main.cpp
+++ Sort/main.cpp
@@ -4,18 +4,24 @@
 #include "SelectionSort.h"
 #include "BubbleSort.h"
 #include "InsertionSort.h"
+#include "MergeSort.h"
 
 using namespace std;
 
 int main (void) {
-    /* Choose sort algorithm */
+    /* ----Choose sort algorithm---- */
     /* SelectionSort Test; */
     /* BubbleSort Test; */
-    InsertionSort Test;
+    /* InsertionSort Test; */
+    MergeSort Test;
 
     ESort E;
-    int data[] = {-100,-1, 0,9,4,7,2,1};
+    int data[] = {-1, 0,9,4,7,2,1, -100};
     int data_size = sizeof(data)/sizeof(int);
+    int data2[data_size] = {0,};
+
+    for(int i = 0; i < data_size ; i++)
+    {data2[i] = data[i];}
 
     cout << "rare data.\n";
     E.printData(data, data_size);
@@ -25,8 +31,8 @@ int main (void) {
     cout << "sort data.\n";
     E.printData(data, data_size);
     cout << "answer data.\n";
-    sort(data, data+data_size);
-    E.printData(data, data_size);
+    sort(data2, data2+data_size);
+    E.printData(data2, data_size);
 
     return 0;
 }
