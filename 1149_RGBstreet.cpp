bbcb1fe4236d5a553caf766e02b33b6e45c0cecd
Kenneth Jung <jungeu1509@gmail.com>
2021-04-23 17:25:17 +0900

    Update 1149_RGBstreet.cpp

--- 1149_RGBstreet.cpp
+++ 1149_RGBstreet.cpp
@@ -6,7 +6,6 @@ using namespace std;
 #define BLUE 2
 
 int map[1001][3];
-int score[1001][3];
 
 int main()
 {
@@ -20,18 +19,18 @@ int main()
 	}
 
 	for(int j = 0; j < 3; j++){
-		score[1][j] = map[1][j];
+		map[1][j] = map[1][j];
 	}
 
 	for(int i = 2; i <= N; i++) {
-		score[i][RED] = ((score[i-1][GREEN] < score[i-1][BLUE]) ? score[i-1][GREEN] : score[i-1][BLUE]) + map[i][RED];
-		score[i][GREEN] = ((score[i-1][RED] < score[i-1][BLUE]) ? score[i-1][RED] : score[i-1][BLUE]) + map[i][GREEN];
-		score[i][BLUE] = ((score[i-1][GREEN] < score[i-1][RED]) ? score[i-1][GREEN] : score[i-1][RED]) + map[i][BLUE];
+		map[i][RED] = ((map[i-1][GREEN] < map[i-1][BLUE]) ? map[i-1][GREEN] : map[i-1][BLUE]) + map[i][RED];
+		map[i][GREEN] = ((map[i-1][RED] < map[i-1][BLUE]) ? map[i-1][RED] : map[i-1][BLUE]) + map[i][GREEN];
+		map[i][BLUE] = ((map[i-1][GREEN] < map[i-1][RED]) ? map[i-1][GREEN] : map[i-1][RED]) + map[i][BLUE];
 	}
 
-	ret = score[N][0];
+	ret = map[N][0];
 	for(int i = 1; i < 3; i++){
-		if(ret > score[N][i]) ret = score[N][i];
+		if(ret > map[N][i]) ret = map[N][i];
 	}
 
 	cout << ret << '\n';
