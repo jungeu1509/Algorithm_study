55ecd87aac90e307f26c8437b52bfc984751923b
Kenneth Jung <jungeu1509@gmail.com>
2021-05-25 21:23:25 +0900

    fix 9095_plus_1_2_3.cpp to DP

--- 9095_plus_1_2_3.cpp
+++ 9095_plus_1_2_3.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 using namespace std;
 
-unsigned int count[12];
+unsigned int data[12];
+
+int myfunc(int A) {
+    if(data[A] > 0) return data[A];
+    return myfunc(A-1) + myfunc(A-2) + myfunc(A-3);
+}
 
 int main(){
-  int N, T;
-  cin >> T;
-  count[1] = 1; // 1
-  count[2] = 2; // 1+1 2
-  count[3] = 4; // 1+1+1 1+2 2+1 3
-
-  for(int i = 4 ; i < 11 ; i++)
-    count[i] = count[i-1] + count[i-2] + count[i-3];
-
-  for(int i = 0 ; i < T; i++){
-    cin >> N;
-    cout << count[N] <<'\n';
-  }
-
-  return 0;
+    int N, T;
+    cin >> T;
+    
+    data[1] = 1; // 1
+    data[2] = 2; // 1+1 2
+    data[3] = 4; // 1+1+1 1+2 2+1 3
+    
+    for(int i = 0 ; i < T ; i++) {
+        cin >> N;
+        cout << myfunc(N) << '\n';
+    }
+    
+    return 0;
 }
