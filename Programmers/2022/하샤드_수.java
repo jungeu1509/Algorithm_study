d47657a8e25cc83a1dd794ff6b1eea92e4375e51
Kenneth Jung <jungeu1509@gmail.com>
2022-02-28 17:51:12 +0900

    refactoring 하샤드_수.java

--- Programmers/2022/하샤드_수.java
+++ Programmers/2022/하샤드_수.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
         int div = x;
         int sum = 0;
         
@@ -8,10 +7,7 @@ class Solution {
             sum += div % 10;
             div /= 10;
         }
-        
-        if(x % sum == 0) answer = true;
-        else answer = false;
-        
-        return answer;
+
+        return (x % sum == 0);
     }
 }
